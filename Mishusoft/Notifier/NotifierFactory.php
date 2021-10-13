b4f1431726568eb8d618495068bc26aa91fd2fad
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-13 16:41:35 +0600

    Create NotifierFactory.php

---
+++ Mishusoft/Notifier/NotifierFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Mishusoft\Notifier;
+
+use Exception;
+
+class NotifierFactory
+{
+    public static function getNotifier($notifier, $to)
+    {
+        if (empty($notifier)) {
+            throw new Exception("No notifier passed.");
+        }
+        switch ($notifier) {
+            case 'SMS':
+                return new SMS($to);
+                break;
+            case 'Email':
+                return new Email($to, 'Junade');
+                break;
+            default:
+                throw new Exception("Notifier invalid.");
+                break;
+        }
+    }
+}
