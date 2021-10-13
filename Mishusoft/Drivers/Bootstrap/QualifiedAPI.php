25e82ac94209fab96a76bf425e046025c62f4cc9
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-13 16:40:19 +0600

    Update QualifiedAPI.php

--- Mishusoft/Drivers/Bootstrap/QualifiedAPI.php
+++ Mishusoft/Drivers/Bootstrap/QualifiedAPI.php
@@ -17,7 +17,6 @@ class QualifiedAPI
 {
 
     /**
-     * @param Http\Request\QualifiedAPI $request
      * @throws Exceptions\LogicException\InvalidArgumentException
      * @throws Exceptions\PermissionRequiredException
      * @throws Exceptions\RuntimeException
@@ -35,126 +34,115 @@ class QualifiedAPI
          * */
 
         //verify core directory
-        if (file_exists(Storage::qualifiedAPIRoutesCoreDirectory()) === true) {
+        if (file_exists(Storage::qualifiedAPIRoutesCoreDirectory())) {
             $rootDirectories[] = Storage::qualifiedAPIRoutesCoreDirectory();
         }
         //verify user defined directory
-        if (file_exists(Storage::qualifiedAPIRoutesDirectory()) === true) {
+        if (file_exists(Storage::qualifiedAPIRoutesDirectory())) {
             $rootDirectories[] = Storage::qualifiedAPIRoutesDirectory();
         }
 
-        if (count($rootDirectories) > 0) {
-            foreach ($rootDirectories as $rootDirectory) {
-                Log::info(sprintf('Check %s directory existent.', $rootDirectory));
-                if (is_readable($rootDirectory) === true) {
-                    Log::info(sprintf('%s directory is readable.', $rootDirectory));
-                    /*
-                     * We need to check Embedded Web Url (Built-In web interface) root path,
-                     * if exists this path,
-                     * The System will be executed all active splitters
-                     * that is located at <APP_DIRECTORY>/embeddedWebUrlDirectory
-                     * */
-                    MPM\Common::autoUpdateQualifiedAPIRoutes($rootDirectory);
+        foreach ($rootDirectories as $rootDirectory) {
+            Log::info(sprintf('Check %s directory existent.', $rootDirectory));
+            if (is_readable($rootDirectory)) {
+                Log::info(sprintf('%s directory is readable.', $rootDirectory));
+                /*
+                 * We need to check Embedded Web Url (Built-In web interface) root path,
+                 * if exists this path,
+                 * The System will be executed all active splitters
+                 * that is located at <APP_DIRECTORY>/embeddedWebUrlDirectory
+                 * */
+                MPM\Common::autoUpdateQualifiedAPIRoutes($rootDirectory);
 
-                    /*
-                     * Load API urls.
-                     * */
-                    Log::info(sprintf('Check routes file in %s directory.', dirname(self::routesFile())));
-                    if (count(Storage\FileSystem\Yaml::parseFile(self::routesFile())) > 0) {
-                        foreach (Storage\FileSystem\Yaml::parseFile(self::routesFile()) as $routeDetails) {
-                            if (is_array($routeDetails) && count($routeDetails) > 0) {
-                                $requestedRoute = Arr::value($routeDetails, 'route');
-                                if (in_array(strtolower($request->getController()), $requestedRoute, true) === true) {
-                                    $requestedClassName = Arr::value($routeDetails, 'class');
-                                    $requestedRouteFile = self::currentFile($rootDirectory, $requestedClassName);
+                /*
+                 * Load API urls.
+                 * */
+                Log::info(sprintf('Check routes file in %s directory.', dirname(self::routesFile())));
+                if (count(Storage\FileSystem\Yaml::parseFile(self::routesFile())) > 0) {
+                    foreach (Storage\FileSystem\Yaml::parseFile(self::routesFile()) as $routeDetails) {
+                        if (is_array($routeDetails) && $routeDetails !== []) {
+                            $requestedRoute = Arr::value($routeDetails, 'route');
+                            if (in_array(strtolower($request->getController()), $requestedRoute, true)) {
+                                $requestedClassName = Arr::value($routeDetails, 'class');
+                                $requestedRouteFile = self::currentFile($rootDirectory, $requestedClassName);
 
+                                if (is_readable($requestedRouteFile)) {
+                                    Log::info(sprintf('Load %s from %s.', $requestedRouteFile, $rootDirectory));
+                                    include_once self::currentFile($rootDirectory, $requestedClassName);
 
-                                    if (is_readable($requestedRouteFile) === true) {
-                                        Log::info(sprintf('Load %s from %s.', $requestedRouteFile, $rootDirectory));
-                                        include_once self::currentFile($rootDirectory, $requestedClassName);
+                                    Log::info(sprintf(
+                                        'Extract %1$s from %2$s.',
+                                        $requestedClassName,
+                                        $requestedRouteFile
+                                    ));
+                                    $urlSplitter = Base::getClassNamespace($requestedRouteFile);
+                                    $methodName = Inflect::lower($request->getController());
+                                    $methodNameFull = sprintf(
+                                        '%1$s::%2$s',
+                                        $urlSplitter,
+                                        Inflect::lower($request->getController())
+                                    );
+                                    $arguments = [
+                                        'controller' => $request->getController(),
+                                        'method' => $request->getMethod(),
+                                        'arguments' => $request->getArguments(),
+                                    ];
 
+                                    if (method_exists(new $urlSplitter(), $methodName)) {
                                         Log::info(sprintf(
-                                            'Extract %1$s from %2$s.',
+                                            'Execute %1$s from %2$s.',
                                             $requestedClassName,
                                             $requestedRouteFile
                                         ));
-                                        $urlSplitter = Base::getClassNamespace($requestedRouteFile);
-                                        $methodName = Inflect::lower($request->getController());
-                                        $methodNameFull = sprintf(
-                                            '%1$s::%2$s',
-                                            $urlSplitter,
-                                            Inflect::lower($request->getController())
-                                        );
-                                        $arguments = [
-                                            'controller' => $request->getController(),
-                                            'method' => $request->getMethod(),
-                                            'arguments' => $request->getArguments(),
-                                        ];
-
-                                        if (method_exists(new $urlSplitter(), $methodName) === true) {
-                                            Log::info(sprintf(
-                                                'Execute %1$s from %2$s.',
-                                                $requestedClassName,
-                                                $requestedRouteFile
-                                            ));
-                                            call_user_func([new $urlSplitter(), $methodName,], $arguments);
-                                            //close framework
-                                            Framework::terminate();
-                                        } else {
-                                            Log::info(sprintf(
+                                        call_user_func([new $urlSplitter(), $methodName,], $arguments);
+                                        //close framework
+                                        Framework::terminate();
+                                    } else {
+                                        Log::info(sprintf(
+                                            'Not found %1$s form %2$s.',
+                                            $methodNameFull,
+                                            $requestedRouteFile
+                                        ));
+                                        throw new Exceptions\RuntimeException\NotFoundException(
+                                            sprintf(
                                                 'Not found %1$s form %2$s.',
                                                 $methodNameFull,
                                                 $requestedRouteFile
-                                            ));
-                                            throw new Exceptions\RuntimeException\NotFoundException(
-                                                sprintf(
-                                                    'Not found %1$s form %2$s.',
-                                                    $methodNameFull,
-                                                    $requestedRouteFile
-                                                )
-                                            );
-                                        }//end if
-                                    } else {
-                                        Log::info(
-                                            sprintf(
-                                                'Not found %1$s.php from %2$s.',
-                                                $requestedClassName,
-                                                $rootDirectory
-                                            )
-                                        );
-                                        throw new Exceptions\RuntimeException\NotFoundException(
-                                            sprintf(
-                                                'Not found %1$s.php from %2$s.',
-                                                $requestedClassName,
-                                                $rootDirectory
                                             )
                                         );
                                     }//end if
+                                } else {
+                                    Log::info(
+                                        sprintf(
+                                            'Not found %1$s.php from %2$s.',
+                                            $requestedClassName,
+                                            $rootDirectory
+                                        )
+                                    );
+                                    throw new Exceptions\RuntimeException\NotFoundException(
+                                        sprintf(
+                                            'Not found %1$s.php from %2$s.',
+                                            $requestedClassName,
+                                            $rootDirectory
+                                        )
+                                    );
                                 }//end if
-                            }
-                        }//end foreach
-                    }//end if
+                            }//end if
+                        }
+                    }//end foreach
                 }//end if
-            }
+            }//end if
         }
 
         //Framework::terminate();
     }
 
 
-    /**
-     * @param string $directory
-     * @param string $requestedClassName
-     * @return string
-     */
     private static function currentFile(string $directory, string $requestedClassName): string
     {
         return sprintf('%1$s%2$s.php', $directory, $requestedClassName);
     }
 
-    /**
-     * @return string
-     */
     private static function routesFile(): string
     {
         return MPM\Common::qualifiedAPIRoutesFile();
