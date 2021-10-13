2bdf29ffbc64068a891bf69ff42f79f0a4fb0fb0
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-13 16:40:34 +0600

    Update MishusoftViewInterface.php

--- Mishusoft/Drivers/View/MishusoftViewInterface.php
+++ Mishusoft/Drivers/View/MishusoftViewInterface.php
@@ -9,43 +9,24 @@ interface MishusoftViewInterface
 
     /**
      * MishusoftViewRenderInterface constructor.
-     *
-     * @param string $hostUrl
-     * @param string $rootTitle
-     * @param array  $noMenuList
-     * @param array  $request
      */
     public function __construct(string $hostUrl, string $rootTitle, array $noMenuList, array $request);
 
 
-    /**
-     * @param array $config
-     */
     public function setWidgetConfig(array $config): void;
 
 
-    /**
-     * @param string $documentTitle
-     */
     public function setDocumentTitle(string $documentTitle): void;
 
 
-    /**
-     * @param string $urlOfHostedWebsite
-     */
     public function setUrlOfHostedWebsite(string $urlOfHostedWebsite): void;
 
 
-    /**
-     * @param array $options
-     */
     public function display(array $options = []): void;
 
 
     /**
-     * @param  string  $tplKey
      * @param  $tplValue
-     * @return array
      */
     public function assign(string $tplKey, $tplValue): array;
 }//end interface
