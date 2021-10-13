14ec2f0d5215e5ec275f57f7865e36ade22ff6c0
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-13 16:40:39 +0600

    Update Handler.php

--- Mishusoft/Exceptions/Handler.php
+++ Mishusoft/Exceptions/Handler.php
@@ -15,6 +15,9 @@ use Mishusoft\Utility\Number;
 class Handler extends ErrorException implements ExceptionInterface
 {
     private string $objectName;
+    /**
+     * @var string|mixed[]
+     */
     private $anotherTrace;
 
     /**
@@ -45,15 +48,9 @@ class Handler extends ErrorException implements ExceptionInterface
             ->addExtra($exception->getTrace(), (new \ReflectionClass($exception))->getShortName())
             ->display();
     }//end fetch()
-
     /**
      * Fetch error on runtime catch area.
      *
-     * @param int $number
-     * @param string $message
-     * @param string $file
-     * @param int $line
-     * @param array $trace
      * @throws AddressNotFoundException
      * @throws HttpException\HttpResponseException
      * @throws InvalidDatabaseException
@@ -70,8 +67,11 @@ class Handler extends ErrorException implements ExceptionInterface
             ->addExtra($trace)
             ->display();
     }//end fetch()
-
-    private function addExtra( $trace = '', string $objectName = '')
+    /**
+     * @param string|mixed[] $trace
+     * @return $this
+     */
+    private function addExtra($trace = '', string $objectName = '')
     {
         $this->anotherTrace = $trace;
         $this->objectName = $objectName;
@@ -93,15 +93,11 @@ class Handler extends ErrorException implements ExceptionInterface
     {
         $description = Storage::hidePath(sprintf('%s from %s on line %d', $this->message, $this->file, $this->line));
 
-        if ($this->objectName !== '') {
-            $titleOfErrorMessage = $this->objectName;
-        } else {
-            $titleOfErrorMessage = self::codeToName($this->getCode());
-        }
+        $titleOfErrorMessage = $this->objectName !== '' ? $this->objectName : self::codeToName($this->getCode());
 
         if ($this->anotherTrace !== '') {
             $stack = self::makeBeautifulStackTrace($this->anotherTrace);
-        } elseif (is_array($this->getTrace()) === true && count($this->getTrace()) > 0) {
+        } elseif (is_array($this->getTrace()) && $this->getTrace() !== []) {
             $stack = self::makeBeautifulStackTrace($this->getTrace());
         } elseif ($this->getTraceAsString() !== '') {
             $stack = self::makeBeautifulStackTrace($this->getTraceAsString());
@@ -139,42 +135,42 @@ class Handler extends ErrorException implements ExceptionInterface
      * @param array|string $trace Error call stack.
      *
      */
-    public static function makeBeautifulStackTrace( $trace): array
+    public static function makeBeautifulStackTrace($trace): array
     {
         //Debug::preOutput($trace);
         $traceArray = [];
         //when trace is array
-        if (is_array($trace) === true && count($trace) > 0) {
+        if (is_array($trace) && $trace !== []) {
             $traceArray = self::cleanTraceArray($trace);
             foreach ($traceArray as $key => $value) {
                 $line = '';
                 //add file name
-                if (array_key_exists('file', $value) === true) {
+                if (array_key_exists('file', $value)) {
                     $line .= $value['file'];
                 } else {
                     $line .= '[internal function]';
                 }
 
                 //add line number
-                if (array_key_exists('line', $value) === true) {
+                if (array_key_exists('line', $value)) {
                     $line .= sprintf('(%1$s)', $value['line']);
                 }
 
                 $line .= ': ';
 
                 //add class name
-                if (array_key_exists('class', $value) === true) {
+                if (array_key_exists('class', $value)) {
                     $line .= $value['class'];
                 }
 
                 //add separator
-                if (array_key_exists('type', $value) === true) {
+                if (array_key_exists('type', $value)) {
                     $line .= $value['type'];
                 }
 
                 //add function name
-                if (array_key_exists('function', $value) === true) {
-                    if (array_key_exists('args', $value) === true) {
+                if (array_key_exists('function', $value)) {
+                    if (array_key_exists('args', $value)) {
                         $line .= $value['function'];
                         if (count($value['args']) > 0) {
                             $implodeArgument = '';
@@ -197,7 +193,7 @@ class Handler extends ErrorException implements ExceptionInterface
                                             $implodeArgument .=sprintf('%1$s=>%2$s, ', $k, $v);
                                         }
                                     }
-                                    if (str_ends_with($implodeArgument, ', ')) {
+                                    if (substr_compare($implodeArgument, ', ', -strlen(', ')) === 0) {
                                         $implodeArgument = substr($implodeArgument, 0, -2);
                                     }
                                     $implodeArgument .= ']';
@@ -220,7 +216,7 @@ class Handler extends ErrorException implements ExceptionInterface
                                 }
                             }
 
-                            if (str_ends_with($implodeArgument, ', ')) {
+                            if (substr_compare($implodeArgument, ', ', -strlen(', ')) === 0) {
                                 $implodeArgument = substr($implodeArgument, 0, -2);
                             }
                             $line .= sprintf('(%1$s)', $implodeArgument);
@@ -237,7 +233,7 @@ class Handler extends ErrorException implements ExceptionInterface
         }//end if
 
         //when trace is string
-        if (is_string($trace) === true && $trace !== '') {
+        if (is_string($trace) && $trace !== '') {
             $traceArray = self::cleanTraceArray(explode("\n", $trace));
             foreach ($traceArray as $key => $value) {
                 $traceArray[$key] = Storage::hidePath(preg_replace('/[#]\d+/', $key, $value));
@@ -264,33 +260,30 @@ class Handler extends ErrorException implements ExceptionInterface
         $output .= $exception->getFile() . ' on line ' . $exception->getLine() . '.';
         return $output;
     }//end toWriteable()
-
-
     /**
      * Clean call trace.
      *
      * @param array $traceArray Array format of error trace.
      *
-     * @return array
      */
     private static function cleanTraceArray(array $traceArray): array
     {
         foreach ($traceArray as $key => $value) {
-            if (is_array($value) === true) {
-                if (array_key_exists('function', $value) === true) {
-                    if (str_contains($value['function'], '{closure}()') === true) {
+            if (is_array($value)) {
+                if (array_key_exists('function', $value)) {
+                    if (strpos($value['function'], '{closure}()') !== false) {
                         unset($traceArray[$key]);
                     }
-                    if (str_contains($value['function'], '{main}') === true) {
+                    if (strpos($value['function'], '{main}') !== false) {
                         unset($traceArray[$key]);
                     }
                 }
             } else {
-                if (str_contains($value, '{closure}()') === true) {
+                if (strpos($value, '{closure}()') !== false) {
                     unset($traceArray[$key]);
                 }
 
-                if (str_contains($value, '{main}') === true) {
+                if (strpos($value, '{main}') !== false) {
                     unset($traceArray[$key]);
                 }
             }
@@ -300,134 +293,50 @@ class Handler extends ErrorException implements ExceptionInterface
         // ksort($traceArray, SORT_ASC);
         return array_reverse($traceArray);
     }//end cleanTraceArray()
-
     /**
      * Error code to name assignment.
      *
      * @param integer $code Error code.
      *
-     * @return false|string Assigned name.
+     * @return bool|string Assigned name.
      */
     public static function codeToName(int $code)
     {
-        /*
-         * Error Levels in PHP
-         * Usually, whenever the PHP engine encounters a problem that
-         * prevents a script from running properly it generates an error message.
-         * There are sixteen different error levels and each level is represented
-         * by an integer value and an associated constant.
-         */
-
-        // Define an assoc array of error string in reality the only entries we should
-        // consider are E_WARNING, E_NOTICE, E_USER_ERROR, E_USER_WARNING and E_USER_NOTICE.
-        return match ($code) {
-            //Error Level: E_ERROR
-            //Value: 1
-            //Description:  A fatal run-time error, that can't be recovered from.
-            // The execution of the script is stopped immediately.
-            1 => 'Critical Error',
-
-            //Error Level: E_WARNING
-            //Value: 2
-            //Description:  A run-time warning. It is non-fatal and most errors tend to fall into this category.
-            // The execution of the script is not stopped.
-            2 => 'Warning',
-
-            //Error Level: E_PARSE
-            //Value: 4
-            //Description:  The compile-time parse error. Parse errors should only be generated by the parser.
-            4 => 'Parse Error',
-
-            //Error Level: E_NOTICE
-            //Value: 8
-            //Description:  A run-time notice indicating that
-            // the script encountered something that could possibly an error,
-            // although the situation could also occur when running a script normally.
-            8 => 'Notice',
-
-            //Error Level: E_CORE_ERROR
-            //Value: 16
-            //Description:  A fatal error that occur during the PHP's engine initial startup.
-            // This is like an E_ERROR, except it is generated by the core of PHP.
-            16 => 'Fatal Error',
-
-            //Error Level: E_CORE_WARNING
-            //Value: 32
-            //Description:  A non-fatal error that occur during the PHP's engine initial startup.
-            // This is like an E_WARNING, except it is generated by the core of PHP.
-            32 => 'Core Warning',
-
-            //Error Level: E_COMPILE_ERROR
-            //Value: 64
-            //Description:  A fatal error that occur while the script was being compiled.
-            // This is like an E_ERROR, except it is generated by the Zend Scripting Engine.
-            64 => 'Compile Error',
-
-            //Error Level: E_COMPILE_WARNING
-            //Value: 128
-            //Description:  A non-fatal error occur while the script was being compiled.
-            // This is like an E_WARNING, except it is generated by the Zend Scripting Engine.
-            128 => 'Compile Warning',
-
-            //Error Level: E_USER_ERROR
-            //Value: 256
-            //Description:  A fatal user-generated error message.
-            // This is like an E_ERROR,
-            // except it is generated by the PHP code using the function trigger_error() rather than the PHP engine.
-            256 => 'User Error',
-
-            //Error Level: E_USER_WARNING
-            //Value: 512
-            //Description:  A non-fatal user-generated warning message.
-            // This is like an E_WARNING,
-            // except it is generated by the PHP code using the function trigger_error() rather than the PHP engine
-            512 => 'User Warning',
-
-            //Error Level: E_USER_NOTICE
-            //Value: 1024
-            //Description:  A user-generated notice message.
-            // This is like an E_NOTICE,
-            // except it is generated by the PHP code using the function trigger_error() rather than the PHP engine.
-            1024 => 'User Notice',
-
-            //Error Level: E_STRICT
-            //Value: 2048
-            //Description:  Not strictly an error,
-            // but triggered whenever PHP encounters code that could lead to problems or forward incompatibilities
-            2048 => 'Runtime Notice',
-
-            //Error Level: E_RECOVERABLE_ERROR
-            //Value: 4096
-            //Description:  A catchable fatal error. Although the error was fatal,
-            // it did not leave the PHP engine in an unstable state.
-            // If the error is not caught by a user defined error handler (see set_error_handler()),
-            // the application aborts as it was an E_ERROR.
-            4096 => 'Catchable Fatal Error',
-
-            //Error Level: E_DEPRECATED
-            //Value: 8192
-            //Description:  A run-time notice indicating that the code will not work in future versions of PHP
-            8192 => 'Identification',
-
-            //Error Level: E_USER_DEPRECATED
-            //Value: 16384
-            //Description:  A user-generated warning message. This is like an E_DEPRECATED,
-            //  except it is generated by the PHP code using the function trigger_error() rather than the PHP engine.
-            16384 => 'User Identification',
-
-            //Error Level: _ALL
-            //Value: 32767
-            //Description:  All errors and warnings, except of level E_STRICT prior to PHP 5.4.0.
-            32767 => '_ALL',
-
-            default => 'Unexpected Error',
-        };//end match
-    }//end codeToName()
-
-    /**
-     * Destruct runtime errors class.
-     */
-    public function __destruct()
-    {
+        switch ($code) {
+            case 1:
+                return 'Critical Error';
+            case 2:
+                return 'Warning';
+            case 4:
+                return 'Parse Error';
+            case 8:
+                return 'Notice';
+            case 16:
+                return 'Fatal Error';
+            case 32:
+                return 'Core Warning';
+            case 64:
+                return 'Compile Error';
+            case 128:
+                return 'Compile Warning';
+            case 256:
+                return 'User Error';
+            case 512:
+                return 'User Warning';
+            case 1024:
+                return 'User Notice';
+            case 2048:
+                return 'Runtime Notice';
+            case 4096:
+                return 'Catchable Fatal Error';
+            case 8192:
+                return 'Identification';
+            case 16384:
+                return 'User Identification';
+            case 32767:
+                return '_ALL';
+            default:
+                return 'Unexpected Error';
+        }//end match
     }//end __destruct()
 }//end class
